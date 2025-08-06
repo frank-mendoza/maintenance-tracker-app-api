5051d37d1dd73efb0a8378af24bc1f1fd1d21536
frankmendoza101 <frnkmendoza101@gmail.com>
2025-08-06 22:58:18 +0800

    Remove dist

--- dist/utils/paginationAndSort.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getPaginationAndSort = void 0;
-const getPaginationAndSort = ({ sort, page = 1, limit = 10, sortOptions = {}, }) => {
-    const sortKey = sortOptions[sort || ""] || sortOptions["newest"];
-    const skip = (page - 1) * limit;
-    return { sortKey, skip, limit };
-};
-exports.getPaginationAndSort = getPaginationAndSort;
