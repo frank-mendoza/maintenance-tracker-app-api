ee50f5e834f2edd11db4589ed67273b9256d9533
frankmendoza101 <frnkmendoza101@gmail.com>
2025-08-07 11:54:18 +0800

    fixed active connection  lable

--- src/index.ts
+++ src/index.ts
@@ -103,7 +103,7 @@ mongoose
   .connect(process.env.MONGO_URI!)
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`🌐 Server running at http://localhost:${PORT}`);
+      console.log(`🌐 Server is now active...`);
     });
   })
   .catch((err) => {
