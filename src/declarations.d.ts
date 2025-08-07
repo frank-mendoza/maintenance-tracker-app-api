cc28a0a690b066a1f7fdb2a34b50ade6fe397d35
frankmendoza101 <frnkmendoza101@gmail.com>
2025-08-07 10:40:27 +0800

    fixed deployment issue

---
+++ src/declarations.d.ts
@@ -0,0 +1,8 @@
+declare module "resend";
+declare module "bcryptjs";
+declare module "express-mongo-sanitize";
+declare module "express-rate-limit";
+declare module "express-validator";
+declare module "cors";
+declare module "express";
+declare module "jsonwebtoken";
